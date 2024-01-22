245a0c807791d673a015591800b943673f8a28c4
takubii <vicsat93@gmail.com>
2024-01-22 16:24:08 +0900

    add: revenueコンポーネントを追加し、ダッシュボードに表示

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { fetchRevenue } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
