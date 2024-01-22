016b296be69a9b6758b1766bad2468ba1a7d1874
takubii <vicsat93@gmail.com>
2024-01-22 16:49:22 +0900

    add: LatestInvoiceコンポーネントを追加し、ダッシュボードに表示

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import { fetchRevenue } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
-import RevenueChart from '../ui/dashboard/revenue-chart';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <main>
@@ -13,6 +15,7 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
