7a766d5b1a57cb56551c4d7f22fe8d4865d16abe
takubii <vicsat93@gmail.com>
2024-01-30 13:27:29 +0900

    update: Cardコンポーネントも遅延読み込みをするように変更

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   UserGroupIcon,
 } from '@heroicons/react/24/outline';
 
+import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
@@ -13,6 +14,28 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
 
 export function Card({
   title,
